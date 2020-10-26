36e3f7396ac34093bd40503896cbfcfac690a586
MatthewCroughan <matthew.croughan@foundries.io>
2020-10-26 18:04:06 +0000

    v0.12 release

--- Formula/fioctl.rb
+++ Formula/fioctl.rb
@@ -1,16 +1,16 @@
 class Fioctl < Formula
   desc "A simple tool to interact with the Foundries.io REST API for managing a Factory."
   homepage "https://foundries.io"
-  version "0.11"
+  version "0.12"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/foundriesio/homebrew-fioctl/releases/download/v0.11/fioctl_0.11_Darwin_x86_64.tar.gz"
-    sha256 "ec78dc26c5132c52ca6956b6f206d1a0dc10746036e9b7b46b6fdbeef5916593"
+    url "https://github.com/foundriesio/homebrew-fioctl/releases/download/v0.12/fioctl_0.12_Darwin_x86_64.tar.gz"
+    sha256 "c15a6e8252cee902e4225461ac77e17d0bf99a98dc597c98d6ae5c8d7e049d9d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/foundriesio/homebrew-fioctl/releases/download/v0.11/fioctl_0.11_Linux_x86_64.tar.gz"
-      sha256 "b7902c555afbe6829ed3446f9244624f67f68ffb51241d46db1c376c08b6b3de"
+      url "https://github.com/foundriesio/homebrew-fioctl/releases/download/v0.12/fioctl_0.12_Linux_x86_64.tar.gz"
+      sha256 "e412a01e865716ee1c9f2b0de5b99bdb5cc3bdab46fee86b8b8a04d39be72445"
     end
   end
 
